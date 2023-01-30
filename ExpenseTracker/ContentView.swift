acb341b2918336893cb5b61d1c3b64a4ee54dbdc
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2023-01-29 23:10:40 -0800

    add main page

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body:some View{
         NavigationView{
-           
+            ScrollView{
+                
+            }.background(Color.background)
+                .navi
         }.navigationViewStyle(.stack)
     }
 }
